f28b0197f5e3bd4c6093288e891237482d3350e6
Khun Kyaw Kham Hti <khamhti@agga.io>
2024-03-23 12:44:29 +0630

    footer and side bar footer

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React from "react";
+import WomanImg from "../img/woman_hero.png";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section
+      className="bg-red-200 h-[800px] bg-hero
+      bg-no-repeat bg-cover bg-center py-24"
+    >
+      <div
+        className="container mx-auto flex
+      h-full justify-around"
+      >
+        {/* text */}
+        <div className="flex flex-col justify-center">
+          <div
+            className="font-semibold uppercase flex
+            items-center"
+          >
+            <div
+              className="bg-red-500 mr-3 h-[2px] 
+              w-10"
+            ></div>
+            New Trend
+          </div>
+          <h1
+            className="text-[70px] leading-[1.1]
+            font-light mb-4"
+          >
+            AUTUMN SALE STYLISH <br />
+            <span className="font-semibold">WOMEN</span>
+          </h1>
+          <Link
+            to={"/"}
+            className="self-start uppercase
+            font-semibold border-b-2 border-primary"
+          >
+            Discover More
+          </Link>
+        </div>
+        {/* img */}
+        <div className="lg:block hidden">
+          <div>
+            <img src={WomanImg} alt="Woman" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -12,55 +12,74 @@ const Sidebar = () => {
   console.log(total);
 
   return (
-    <div
-      className={`${
-        isOpen ? "right-0" : "-right-full"
-      } w-full bg-white fixed top-0 h-full shadow-2xl 
+    <>
+      <div
+        className={`${
+          isOpen ? "right-0" : "-right-full"
+        } w-full bg-white fixed top-0 h-full shadow-2xl 
       md:w-[35vw] 
         xl:max-w-[30vw] transition-all duration-300 z-20 
         px-4 lg:px-[35px]`}
-    >
-      <div className="flex justify-between items-center py-6 border-b">
-        <div className="font-semibold text-sm uppercase">Shopping Bag (0)</div>
-        <div
-          onClick={handleClose}
-          className="w-8 h-8 cursor-pointer 
+      >
+        <div className="flex justify-between items-center py-6 border-b">
+          <div className="font-semibold text-sm uppercase">
+            Shopping Bag (0)
+          </div>
+          <div
+            onClick={handleClose}
+            className="w-8 h-8 cursor-pointer 
           flex justify-center items-center"
-        >
-          <IoMdArrowForward className="text-2xl" />
+          >
+            <IoMdArrowForward className="text-2xl" />
+          </div>
         </div>
-      </div>
-      <div
-        className="flex flex-col gap-y-2 h-[520px] 
+        <div
+          className="flex flex-col gap-y-2 h-[520px] 
         lg:h-[640px] overflow-y-auto overflow-x-hidden 
         border-b"
-      >
-        {cart.map((item) => {
-          return <CartItem item={item} key={item.id} />;
-        })}
-      </div>
-
-      {/*  sidebar bottom */}
-      <div className="flex flex-col gap-y-3 mt-4">
-        <div
-          className="flex w-full 
-        justify-between items-center"
         >
-          {/* total */}
-          <div className="uppercase font-semibold">
-            <span className="mr-2">Total:</span>$ {parseFloat(total).toFixed(2)}
-          </div>
-          {/* clear cart icon */}
+          {cart.map((item) => {
+            return <CartItem item={item} key={item.id} />;
+          })}
+        </div>
+
+        {/*  sidebar bottom */}
+        <div className="flex flex-col gap-y-3">
           <div
-            onClick={clearCart}
-            className="cursor-pointer py-4 bg-rose-500 w-12 h-12 
-            flex justify-center items-center text-xl text-white"
+            className="flex w-full 
+          justify-between items-center"
           >
-            <FiTrash2 />
+            {/* total */}
+            <div className="uppercase font-semibold">
+              <span className="mr-2">Total:</span>${" "}
+              {parseFloat(total).toFixed(2)}
+            </div>
+            {/* clear cart icon */}
+            <div
+              onClick={clearCart}
+              className="cursor-pointer py-4 bg-rose-500 w-12 h-12 
+            flex justify-center items-center text-xl text-white"
+            >
+              <FiTrash2 />
+            </div>
           </div>
+          <Link
+            to={"/"}
+            className="bg-gray-200 flex w-full font-medium
+        text-primary p-4 justify-center items-center"
+          >
+            View Cart
+          </Link>
+          <Link
+            to={"/"}
+            className="bg-gray-200 flex w-full font-medium
+        text-primary p-4 justify-center items-center"
+          >
+            Checkout
+          </Link>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
